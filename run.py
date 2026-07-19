a72e8ffb1a210e2a9ec133fdf856ff987e1b18eb
pranavmtn <lyractk@outlook.com>
2026-07-19 15:32:37 -0400

    Change default port from 5000 to 8080

--- run.py
+++ run.py
@@ -9,6 +9,6 @@ app = create_app(os.getenv("FLASK_ENV", "development"))
 if __name__ == "__main__":
     app.run(
         host=os.getenv("FLASK_HOST", "0.0.0.0"),
-        port=int(os.getenv("FLASK_PORT", "5000")),
+        port=int(os.getenv("FLASK_PORT", "8080")),
         debug=os.getenv("FLASK_DEBUG", "1") == "1",
     )
